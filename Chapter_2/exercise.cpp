35beb6e779b52328f701303e04a942adbd40fb2e
StyxXx1337 <kefin87@gmail.com>
2020-12-28 10:07:16 +0900

    Finished Chapter_2 exercises

---
+++ Chapter_2/exercise.cpp
@@ -0,0 +1,19 @@
+#include "std_lib_facilities.h"
+
+int main(){
+    cout << "Hello, programming!\n";
+    cout << "Here we go!\n";
+    cout << "\n=========== How to go to the bathroom ==========\n";
+    cout << "Lift yourself up from the chair until you stand upright.\n";
+    cout << "Turn 90deg counter clockwise.\n";
+    cout << "Make one step to the front.\n";
+    cout << "Turn 90deg counter clockwise.\n";
+    cout << "Make three steps to the front.\n";
+    cout << "Turn 90deg counter clockwise.\n";
+    cout << "Make 3 steps to the front.\n";
+    cout << "Turn 90deg counter clockwise.\n";
+    cout << "Make 3 steps to the front.\n";
+    cout << "You have reached your destination.\n";
+
+    return 0;
+}
