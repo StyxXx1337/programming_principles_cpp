00b781c0cf4b65f0b68c36beea47c96f36a1d039
StyxXx1337 <kefin87@gmail.com>
2020-12-29 13:41:39 +0900

    Finished Exercise 4 & 5 from chapter 3 page 86.

--- Chapter_3/6_exercise.cpp
+++ Chapter_3/6_exercise.cpp
@@ -14,13 +14,15 @@ int main(){
         cout << "Please input the miles you want to convert: ";
         double miles;
         cin >> miles;
-        cout << "Your input " << miles << "miles is equal to " << miles*miles_to_km << "km.\n";
+        cout << "Your input " << miles << "miles is equal to " 
+            << miles*miles_to_km << "km.\n";
     }
     else if (user_choice == 2){
         cout << "Please input the kilometers you want to convert: ";
         double kilometers;
         cin >> kilometers;
-        cout << "Your input " << kilometers << "km is equal to " << kilometers/miles_to_km << "miles.\n";
+        cout << "Your input " << kilometers << "km is equal to " 
+            << kilometers/miles_to_km << "miles.\n";
     }
     else{
         cout << "Please input a valid choice";
