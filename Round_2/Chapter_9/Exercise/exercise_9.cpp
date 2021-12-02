d925753a9f7da679220c89a581d2c3031d88f286
StyxXx1337 <kefin87@gmail.com>
2021-12-02 09:48:00 +0900

    finished exercise 9 & 10

--- Round_2/Chapter_9/Exercise/exercise_9.cpp
+++ Round_2/Chapter_9/Exercise/exercise_9.cpp
@@ -53,6 +53,7 @@ public:
   string get_title() const { return title; };
   string get_isbn() const { return isbn; };
   Genre get_genre() const { return genre; };
+  bool is_checked_out() const { return checked_out; };
   int get_pages() const { return pages; };
 
 // Operators
@@ -93,6 +94,7 @@ Book::Book(string t, string a, string i, Date d, Genre g, int p)
   :title{t}, author{a}, isbn{i}, date{d}, genre{g}, pages{p}
   {
     if (!is_valid_isbn(i)) error("invalid ISBN.");
+    checked_out = false;
 }
 
 bool Book::is_valid_isbn(string i){
@@ -215,15 +217,19 @@ bool Library::book_in_lib(const Book& b) const{
 }
 
 bool Library::checkout_book(Patron p, Book b){
-  if (!patron_in_lib(p)) return false;
-  if (!book_in_lib(b)) return false;
+  if (!patron_in_lib(p)) error("No such Patron");
   if (p.open_fees()) error("Open fees");
+  
+  if (!book_in_lib(b)) error("Book not in Library");
+  if (b.is_checked_out()) error("Book checked out");
+  
   /**
-  * TODO: @Kevin: checkout book
+  * TODO: @Kevin  checkout book
+  * Book in the Library::books vector needs to be modified.
   */
 
   Date d {1800,9,9};
-
+  b.check_out(); // book in books-vector will not be changed.
   transactions.push_back(Transaction{b,p,d});
   return true;
 }
@@ -234,7 +240,7 @@ ostream& operator<<(ostream& os, const Genre g){
   return os << Gen_string[int(g)];
 }
 
-// Part of exercise 6.
+// Book operator<< overload
 ostream& operator<<(ostream& os, const Book& b){
   return os << "Title: \t\t" << b.get_title() << endl
             << "Author: \t" << b.get_author() << endl
@@ -250,7 +256,7 @@ ostream& operator<<(ostream& os, Patron p){
             << "Open Fees: \t" << p.fees();
 }
 
-// Patron operator<< overload
+// Transaction operator<< overload
 ostream& operator<<(ostream& os, Transaction t){
   return os << "Book Author: \t\t" << t.book().get_author() << endl
             << "Patron Name: \t\t" << t.patron().name() << endl
@@ -258,32 +264,51 @@ ostream& operator<<(ostream& os, Transaction t){
 }
 
 int main(){
+
+  /** Testing the Libraray **/
+  // Make Users
   Patron p1 {"Adam", 1, 0.0};
   Patron p2 {"Betty", 2, 1.0};
   Patron p3 {"Carlos", 3, 2.0};
-  Patron p4 {"Dirk", 4, 3.0};
+  Patron p4 {"Dirk", 4, 0.0};
+  Patron p5 {"Thief", 99, 0.0};
 
-  Library L;
-
-  L.add_patron(p1);
-  L.add_patron(p2);
-  L.add_patron(p3);
-  L.add_patron(p4);
-  
   //Book(string t, string a, string i, Date d, Genre g, int p)
   Book b1 {"Book A", "Author A", "1-1-1-A", Date {1850,5,5}, Genre::biography, 100};
   Book b2 {"Book B", "Author B", "1-1-2-B", Date {1900,5,15}, Genre::fiction, 120};
   Book b3 {"Book C", "Author C", "1-1-3-C", Date {1950,5,25}, Genre::nonfiction, 130};
   Book b4 {"Book D", "Author D", "1-1-4-D", Date {2000,6,5}, Genre::periodical, 140};
+  Book b5 {"Not in Lib", "Author E", "1-1-5-D", Date {2011,6,5}, Genre::fiction, 999};
 
+  // Make a library
+  Library L;
+
+  // Adding Users and Books to the Library
+  L.add_patron(p1);
+  L.add_patron(p2);
+  L.add_patron(p3);
+  L.add_patron(p4);
+  
   L.add_book(b1);
   L.add_book(b2);
   L.add_book(b3);
   L.add_book(b4);
   
+  // Checkout Books
   L.checkout_book(p1, b4);
-  L.checkout_book(p2, b4);
-  cout << L.get_transactions()[0] << endl;
+  L.checkout_book(p1, b3);
+  L.checkout_book(p4, b4);
 
+  // See the transactions...
+  cout << L.get_transactions()[0] << endl;
+  cout << L.get_transactions()[1] << endl;
+  cout << L.get_transactions()[2] << endl;
+
+  // Throw Error:
+  /*
+  L.checkout_book(p3, b1); // has open fees
+  L.checkout_book(p5, b1); // Patron not in Lib
+  L.checkout_book(p1, b5); // Book not in Lib
+  */
   return 0;
 }
