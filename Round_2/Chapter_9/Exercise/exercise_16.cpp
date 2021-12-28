f5142a6bdf38b7c13dce34be2b2aa3cc8b45cc2e
StyxXx1337 <kefin87@gmail.com>
2021-12-28 11:06:37 +0900

    Exercise 16 done

---
+++ Round_2/Chapter_9/Exercise/exercise_16.cpp
@@ -0,0 +1,155 @@
+#include "exercise_15.hpp"
+
+Money::Money(double cents, std::string currency)
+{
+  _cents = int(cents + 0.5);
+  if (valid_currency(currency) == true)
+  {
+    _currency = currency;
+  }
+  else
+  {
+    throw std::runtime_error("Currency not supported");
+    // std::cout << "Invalid currency..." << std::endl;
+  }
+}
+
+Money::Money(int dollars, double cents, std::string currency)
+{
+  _cents = dollars * get_factor() + int(cents + 0.5);
+  if (valid_currency(currency) == true)
+  {
+    _currency = currency;
+  }
+  else
+  {
+    throw std::runtime_error("Currency not supported");
+    //std::cout << "Invalid currency..." << std::endl;
+  }
+}
+
+std::ostream &operator<<(std::ostream &os, const Money &m)
+{
+  return os << int(m.get_cents() / m.get_factor())
+            << '.' << abs((m.get_cents() % m.get_factor())) << " "
+            << m.get_currency();
+}
+
+Money operator+(const Money &lhs, const Money &rhs)
+{
+  std::string r_curr = rhs.get_currency(); 
+  std::string l_curr = lhs.get_currency();
+  
+  Money res{0, l_curr};
+  res._cents = lhs._cents + 
+              (
+                (rhs._cents) / 
+                rhs.get_curr_factor(r_curr) * 
+                lhs.get_curr_factor(l_curr)
+              );
+
+  return res;
+}
+
+Money operator-(const Money &lhs, const Money &rhs)
+{
+  std::string r_curr = rhs.get_currency(); 
+  std::string l_curr = lhs.get_currency();
+
+  Money res{0, l_curr};
+  res._cents = lhs._cents - 
+              (
+                (rhs._cents) / 
+                rhs.get_curr_factor(r_curr) * 
+                lhs.get_curr_factor(l_curr)
+              );
+
+  return res;
+}
+
+Money operator*(const Money &lhs, int i)
+{
+  Money res{0, lhs.get_currency()};
+  res._cents = lhs._cents * i;
+
+  return res;
+}
+
+Money operator/(const Money &lhs, int i)
+{
+  Money res{0, lhs.get_currency()};
+  res._cents = lhs._cents / i;
+
+  return res;
+
+}
+
+double Money::get_curr_factor(std::string curr) const
+{
+
+  for (int i; i < supported_currencies.size(); i++){
+    if (supported_currencies[i] == curr){
+      return currency_factors[i];
+    }
+  }
+  return -1;
+}
+
+bool Money::valid_currency(std::string curr){
+  for (std::string c : supported_currencies){
+    if (c == curr){
+      return true;
+    }
+  }
+  return false;
+}
+
+std::istream& operator>>(std::istream& is, Money& m){
+  char c1, c2, c3;
+  double amount = 0;
+  std::string currency = "";
+  
+  is >> c1 >> c2 >> c3 >> amount;
+  if (!is) return is;
+  
+  // Not pretty but works...
+  currency += c1;
+  currency += c2;
+  currency += c3;
+
+  if (m.valid_currency(currency) == true){
+    m = Money(amount, currency);
+  }
+  else{
+    std::cout << "ERROR - Entered Currency not supported" << std::endl;
+    is.clear(std::ios_base::failbit);
+  }
+  return is;
+}
+
+int main()
+{
+
+  Money m1{150.5, "USD"};
+  Money m2{900, 20.4, "DKK"};
+  Money jpy{11000, "JPY"};
+  Money usd{100, "USD"};
+  Money dkk{500, "DKK"};
+
+  std::cout << m1 << std::endl;
+  std::cout << m2 << std::endl;
+  std::cout << m1 << " + " << m2 << " = " << m1 + m2 << std::endl;
+  std::cout << m1 << " - " << m2 << " = " << m1 - m2 << std::endl; // Only one minus...
+  std::cout << m1 << " * " << 3 << " = " << m1 * 3 << std::endl;
+  std::cout << m2 << " / " << 3 << " = " << m2 / 3 << std::endl;
+  std::cout << jpy << " + " << usd << " = " << jpy + usd << std::endl;
+  std::cout << dkk << " - " << usd << " = " << dkk - usd << std::endl;
+  std::cout << usd << " + " << dkk << " = " << usd + dkk << std::endl;
+  Money jpy_input{0, "USD"}; 
+  //std::string temp = "JPY100";
+  std::cin >> jpy_input; 
+  std::cout << jpy_input << std::endl;
+  // std::cout << m1 << " * " << 3 << " = " << m2 / m1 << std::endl; // Won't compile.
+
+  return 0;
+}
---
+++ Round_2/Chapter_9/Exercise/exercise_16.hpp
@@ -0,0 +1,40 @@
+#ifndef _MONEY_H_
+#define _MONEY_H_
+
+#include <ostream>
+#include <istream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Money{
+public:
+  class Invalid { };
+
+  Money(double cents, std::string currency);
+  Money(int dollars, double cents, std::string currency);
+  
+
+  int get_factor(void) const { return _factor; };
+  std::string get_currency(void) const { return _currency; };
+  double get_curr_factor(std::string curr) const;
+  long int get_cents(void) const { return _cents; };
+  void set_cents(int c) { _cents = c; };
+  void set_currency(std::string curr) { _currency = curr; };
+  bool valid_currency(std::string curr);
+
+  friend Money operator+(const Money& lhs, const Money& rhs);
+  friend Money operator-(const Money& lhs, const Money& rhs);
+  friend Money operator*(const Money& lhs, int i);
+  friend Money operator/(const Money& lhs, int i);
+  friend std::istream& operator>>(std::istream& is, Money& m);
+
+private:
+  std::vector<std::string> supported_currencies {"USD", "DKK", "JPY"};
+  std::vector<double> currency_factors {1.0, 5.0, 110.0};
+  long int _cents;
+  std::string _currency = "USD";
+  int _factor = 100;
+};
+
+#endif // _MONEY_H_
