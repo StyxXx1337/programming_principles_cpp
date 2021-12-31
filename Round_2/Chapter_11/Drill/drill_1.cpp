c8ded6f3a4b7f4510f49eba5eaa24c0762b6e427
StyxXx1337 <kefin87@gmail.com>
2021-12-31 22:19:06 +0900

    Drills Chapter 11 finished

---
+++ Round_2/Chapter_11/Drill/drill_1.cpp
@@ -0,0 +1,6 @@
+#include "../../../!_Misc/std_lib_facilities.h"
+
+
+int main(){
+  int birth_year {1987};
+}
---
+++ Round_2/Chapter_11/Drill/drill_2.cpp
@@ -0,0 +1,11 @@
+#include "../../../!_Misc/std_lib_facilities.h"
+
+
+int main(){
+  int birth_year {1987};
+
+  std::cout << "Birthyear in dec: " << birth_year << std::endl
+            << "Birthyear in hex: " << hex << birth_year << std::endl
+            << "Birthyear in oct: " << oct << birth_year << std::endl
+            << "Birthyear in dec: " << dec << birth_year << std::endl;
+}
